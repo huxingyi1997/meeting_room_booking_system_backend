ad42d26e7997efd5e6e6a9d48f41de5f9c5fcf30
huxingyi1997 <huxingyi1997@sina.com>
2024-01-30 08:39:46 +0800

    feat: booking in typeorm

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { EmailModule } from 'src/email/email.module';
 import { RedisModule } from 'src/redis/redis.module';
 import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
+import { Booking } from './entities/booking.entity';
 
 @Module({
-  imports: [EmailModule, RedisModule],
+  imports: [TypeOrmModule.forFeature([Booking]), EmailModule, RedisModule],
   controllers: [BookingController],
   providers: [BookingService],
 })
